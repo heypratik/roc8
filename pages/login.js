aa4395f64d72664dcbdc8e9746868d408245e57f
pratikr0810 <pratikr0810@gmail.com>
2024-11-01 13:26:55 +0000

    Fixes

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import { signIn } from "next-auth/react"
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import {AiOutlineLoading3Quarters} from 'react-icons/ai'
 
 export default function Login() {
